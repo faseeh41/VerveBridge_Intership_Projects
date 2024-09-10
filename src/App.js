473b8521bad5cd007c40f993198595ffe2ccbc01
faseeh41 <rajafaseeh41@gmail.com>
2024-09-10 16:10:47 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ProductList />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Default />} />
+        </Routes>
+        <Modal />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,58 @@
+// Footer.jsx
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <p>
+        Coded by{" "}
+        <a
+          href="https://github.com/faseeh41"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          M.Faseeh Hassan
+        </a>
+        , Open sourced on{" "}
+        <a
+          href="https://github.com/MFaseehHassan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        . Hosted on Vercel.
+      </p>
+    </FooterWrapper>
+  );
+};
+
+const FooterWrapper = styled.footer`
+  background: var(--mainBlue);
+  color: var(--mainWhite);
+  text-align: center;
+  padding: 1rem 0;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 1000; /* Ensure the footer is above other content */
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1); /* Add shadow for better separation */
+  
+  p {
+    margin: 0;
+    font-size: 1rem;
+  }
+
+  a {
+    color: var(--mainYellow);
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default Footer;
